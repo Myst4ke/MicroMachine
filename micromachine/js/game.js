28af14b36fb6c7308b8abea80cb7b5dfc3ef83f7
Florian <102389838+Myst4ke@users.noreply.github.com>
2022-12-04 22:54:27 +0100

    Timer setup

---
+++ micromachine/js/game.js
@@ -0,0 +1,16 @@
+
+
+let gameState = 'menu';
+let oldTime = Date.now();
+
+function printTime(){
+    ctx.font = '48px serif';
+    let date = (Date.now() - oldTime)/1000;
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillText(date.toString(), 350, 580);
+}
+
+
+function drawGameState(){
+
+}
